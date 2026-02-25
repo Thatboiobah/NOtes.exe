ab87f35b31e5a28dcf6c77a885a311f73b2267b1
Obaloluwa <osmpopoola@gmail.com>
2026-02-26 00:32:58 +0100

    Update Notes.jsx

---
+++ notes.exe/src/Lectures.jsx
@@ -0,0 +1,53 @@
+function Lectures({
+  selectedClass,
+  setIsModalOpen,
+  setModalType,
+  setEditingItem,
+  setAppData
+}) {
+  const deleteLecture = (id) => {
+    setAppData((prev) => ({
+      ...prev,
+      classes: prev.classes.map((cls) =>
+        cls.id === selectedClass.id
+          ? {
+              ...cls,
+              lectures: cls.lectures.filter((lec) => lec.id !== id)
+            }
+          : cls
+      )
+    }));
+  };
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setModalType("addLecture");
+          setIsModalOpen(true);
+        }}
+      >
+        + Add Lecture
+      </button>
+
+      {selectedClass.lectures.map((lec) => (
+        <div key={lec.id}>
+          <h4>{lec.topic}</h4>
+          <p>{lec.datetime}</p>
+          <button
+            onClick={() => {
+              setEditingItem(lec);
+              setModalType("editLecture");
+              setIsModalOpen(true);
+            }}
+          >
+            Edit
+          </button>
+          <button onClick={() => deleteLecture(lec.id)}>Delete</button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Lectures;
---
+++ notes.exe/src/Notes.jsx
@@ -0,0 +1,67 @@
+function Notes({
+  selectedClass,
+  setIsModalOpen,
+  setModalType,
+  setEditingItem,
+  setAppData
+}) {
+
+  const deleteNote = (id) => {
+    setAppData((prev) => ({
+      ...prev,
+      classes: prev.classes.map((cls) =>
+        cls.id === selectedClass.id
+          ? {
+              ...cls,
+              notes: cls.notes.filter((note) => note.id !== id)
+            }
+          : cls
+      )
+    }));
+  };
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setEditingItem(null);
+          setModalType("addNote");
+          setIsModalOpen(true);
+        }}
+      >
+        + Add Note
+      </button>
+
+      {selectedClass.notes.length === 0 && (
+        <p>No notes yet.</p>
+      )}
+
+      {selectedClass.notes.map((note) => (
+        <div key={note.id}>
+          <h4>{note.title}</h4>
+          <p>
+            {note.content.length > 100
+              ? note.content.substring(0, 100) + "..."
+              : note.content}
+          </p>
+
+          <button
+            onClick={() => {
+              setEditingItem(note);
+              setModalType("editNote");
+              setIsModalOpen(true);
+            }}
+          >
+            Edit
+          </button>
+
+          <button onClick={() => deleteNote(note.id)}>
+            Delete
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Notes;
