ed8f565cb3fc56acd5496aba90b4714f2f97db8a
Obaloluwa <osmpopoola@gmail.com>
2026-02-26 00:11:40 +0100

    Updated Sidebar component

---
+++ notes.exe/src/Sidebar.jsx
@@ -0,0 +1,25 @@
+function Sidebar({
+  classes,
+  selectedClassId,
+  setSelectedClassId,
+  openAddClassModal
+}) {
+  return (
+    <aside>
+      <ul>
+        {classes.map((cls) => (
+          <li
+            key={cls.id}
+            onClick={() => setSelectedClassId(cls.id)}
+          >
+            {cls.name}
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={openAddClassModal}>+ Add Class</button>
+    </aside>
+  );
+}
+
+export default Sidebar;
