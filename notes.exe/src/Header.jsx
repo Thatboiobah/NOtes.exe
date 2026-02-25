56e6bfd2add56bff4b6d737c456192939dfb9f46
Obaloluwa <osmpopoola@gmail.com>
2026-02-25 23:51:07 +0100

    Added Components

---
+++ notes.exe/src/Header.jsx
@@ -0,0 +1,24 @@
+
+function Header(){
+    return(
+        <header>
+            <nav>
+                <div>
+                    <img src="#" alt="logo" />
+                </div>
+
+                <div>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#"><img src="#" alt="bell-icon" /></a></li>
+                        <li><a href="#"><img src="#" alt="sche" /></a></li>
+                    </ul>
+                </div>
+            </nav>
+            <hr />
+            <hr />
+        </header>
+    )
+}
+
+export default Header
