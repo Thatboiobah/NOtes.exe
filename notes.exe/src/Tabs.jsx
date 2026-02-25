f33c77a1439306536e97d67b1a836fc37d222634
Obaloluwa <osmpopoola@gmail.com>
2026-02-26 00:22:29 +0100

    Updated Tabs component

---
+++ notes.exe/src/Tabs.jsx
@@ -0,0 +1,12 @@
+function Tabs({ activeTab, setActiveTab }) {
+  return (
+    <div>
+      <button onClick={() => setActiveTab("lectures")}>Lectures</button>
+      <button onClick={() => setActiveTab("notes")}>Notes</button>
+      <button onClick={() => setActiveTab("assignments")}>Assignments</button>
+      <button onClick={() => setActiveTab("exams")}>Exams</button>
+    </div>
+  );
+}
+
+export default Tabs;
